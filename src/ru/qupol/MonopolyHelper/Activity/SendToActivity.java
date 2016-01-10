775af030daf520a62fc1dc92b5a2081037c389de
Pavel <K_P_I@bk.ru>
2016-01-10 05:19:46 +0300

    send money to self no more available

--- src/ru/qupol/MonopolyHelper/Activity/SendToActivity.java
+++ src/ru/qupol/MonopolyHelper/Activity/SendToActivity.java
@@ -12,6 +12,8 @@ import java.util.List;
 
 /**
  * Created by Pavel on 13.10.2015.
+ *
+ * Players list showed on player send money action
  */
 public class SendToActivity extends PlayersListActivity {
 
@@ -19,18 +21,20 @@ public class SendToActivity extends PlayersListActivity {
     protected void fillPlayers(LinearLayout layout) {
         List<Player> players = Facade.getPlayerDAO().getAll();
         for (final Player player : players) {
+            Bundle extras = getIntent().getExtras();
+            final Player sender = Facade.getPlayerDAO().get(extras.getInt("senderPlayerId"));
+            final int takeCount = extras.getInt("sendValue");
             RelativeLayout relativeLayout = createPlayerBar(player.getName(), player.getBalance(), new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Bundle extras = getIntent().getExtras();
-                    Player sender = Facade.getPlayerDAO().get(extras.getInt("senderPlayerId"));
-                    int takeCount = extras.getInt("sendValue");
                     sender.sendBalanceTo(player, takeCount);
                     setResult(RESULT_OK);
                     finish();
                 }
             });
-            layout.addView(relativeLayout);
+            if (player == sender) {
+                layout.addView(relativeLayout);
+            }
         }
 
     }
